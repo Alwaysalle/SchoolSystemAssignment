459584bc90e17caf26eee11299b9974091b317fc
Mikael Larsson <mikael.larsson@romram.se>
2016-04-30 14:41:19 +0200

    Added implementetation of tests.

--- SchoolSystem/src/test/java/unit/otherclasses/RoomTest.java
+++ SchoolSystem/src/test/java/unit/otherclasses/RoomTest.java
@@ -1,10 +1,17 @@
 package unit.otherclasses;
 
+import org.junit.Test;
+import otherclasses.Room;
+
 import static org.junit.Assert.*;
 
-/**
- * Created by micke on 2016-04-30.
- */
+
 public class RoomTest {
 
+    @Test
+    public void testRoom() {
+        Room room = new Room(12);
+        assertEquals(12, room.getId());
+        assertEquals(12, room.getId());
+    }
 }
